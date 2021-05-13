024eb463d4bc72cd1cd857e7a447abfcbca563bb
Luis Martinez <lmr@gmail.com>
2021-05-13 09:27:28 +0200

    Version 3. NUevos texto en pantalla

--- inicio.php
+++ inicio.php
@@ -1,7 +1,7 @@
 <h1>APLICACIÓN WEB CICLOS FP</h1>
 <?php
     echo "<h2>ENTORNOS DE DESARROLLO</h2>";
-    echo "Esta es mi primera página de php!";
+    
     echo "HOY hemos aprendido GIT!";
     echo "hemos empezado, mejor dicho!";
 
